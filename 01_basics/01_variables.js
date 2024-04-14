ecdaf988907413f416a0e392072b2c26cdad8001
sonali21112001 <96690254+sonali21112001@users.noreply.github.com>
2024-04-14 16:38:04 +0000

    variables topic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+//userinfo 
+const accountId = 12345;
+//const not change 
+let accoutEmail = "Sonali@gmail.com";
+var accountPassword = "12@sonal";
+accountCity = "Pune";
+let accountState ;
+
+
+// accountId = 12;
+accoutEmail = "sonali@12gmail.com";
+accountPassword = "13@sonali";
+accountCity = "Benguluru";
+
+/*
+ dont use var 
+ because let is use for block scope  
+*/ 
+
+console.table([accountId , accoutEmail ,accountPassword ,accountCity ,accountState]);
+
