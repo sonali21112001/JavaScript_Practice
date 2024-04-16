1b6605d6e39876e01e7c2968c0342d4c76c38676
sonali21112001 <96690254+sonali21112001@users.noreply.github.com>
2024-04-16 19:20:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+// let Number = 3
+// let Name = "sona"
+// let Log = true;
+
+
+//datatypes
+ /*
+ Number 
+ Boolean
+ String
+ Null
+ Undefined
+ BigInt
+ Symbol => unique
+ Objects
+
+ */ 
+
+ console.log(typeof undefined);
+ console.log(typeof null);
+console.log("sonali");
